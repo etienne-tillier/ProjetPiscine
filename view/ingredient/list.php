493afeb685f2ed290840f4f74446c0ca9204897b
Romain Frezier <romainfrezier@gmail.com>
2021-10-13 20:47:18 +0200

    coloration des messages de confirmations

--- view/ingredient/list.php
+++ view/ingredient/list.php
@@ -4,7 +4,6 @@
     <title>Ingredients</title>
     <link rel="stylesheet" type="text/css" href="style/style_contenu_bd.css">
     <link rel="stylesheet" type="text/css" href="style/style_bouton_gestion_bd.css">
-
 </head>
 <body>
     <div id="corps">
--- view/recette/delete.php
+++ view/recette/delete.php
@@ -1,7 +1,8 @@
-
-<?php
-
-echo "<p>La recette a bien été supprimée. <br>";
-require (File::build_path(array("view", "recette", "list.php")));
-?>
+<link rel="stylesheet" type="text/css" href="style/style_message.css">
+<div class = "message_supp">
+    <?php
+        echo "<p>La recette a bien été supprimée. <br>";
+        require (File::build_path(array("view", "recette", "list.php")));
+    ?>
+</div>
  
