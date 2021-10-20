621a8bf7e00fc6033ac234a2b1cafd62b4e5ab3f
etienne.tillier@etu.umontpellier.fr <etienne.tillier@etu.umontpellier.fr>
2021-10-20 18:04:36 +0200

    a

---
+++ config/Func.php
@@ -0,0 +1,13 @@
+<?php
+
+function genererListeIngredient($tabIngredient){
+    $chaine = "";
+    foreach ($tabIngredient as $ingredient){
+        if (is_a($ingredient,"recette")){
+            
+        }
+    }
+}
+
+
+?>
