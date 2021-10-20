f330a0eb2b51d1e9d69c8789e8e64a55ca674bed
Eya Jelassi <jelassieya9@gmail.com>
2021-10-20 22:14:46 +0200

    fenêtre confirmation v2

--- view/ingredient/delete.php
+++ view/ingredient/delete.php
@@ -1,15 +1,7 @@
 <link rel="stylesheet" type="text/css" href="style/style_message.css">
 <div class = "message_supp">
     <script type="text/javascript">
-        //alert("L'ingrédient a bien été supprimé");
-        if (confirm('Are you sure you want to save this thing into the database?')) {
-  // Save it!
-  console.log('Thing was saved to the database.');
-    } else {
-  // Do nothing!
-  console.log('Thing was not saved to the database.');
-    }
-
+        alert("L'ingrédient a bien été supprimé");
         window.location = 'index.php';
     </script>
     <?php
